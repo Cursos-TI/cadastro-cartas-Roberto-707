c6f933b2a352446277cfd0223fcb2877657d149f
Roberto-707 <robertoraschkejunior@gmail.com>
2025-09-24 16:22:28 +0000

    carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,8 +8,18 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     printf("Desafio SuperTrunfo\n");
   // Área para entrada de dados
+    char estado [10] = 'A';
+    char codigo [3] = 'A01';
+    char cidade [10] = 'Pernambuco';
+    int população = 1300000000;
+    float area = 15000000;
+    float PIB = 130000000;
+    int pontosturisticos = 5;
+
 
   // Área para exibição dos dados da cidade
+    printf("O estado é: %f\n", estado);
 
 return 0;
+
 } 
