c3200c2014e473e9fd973c2fe420d3f4a5666c23
Roberto-707 <robertoraschkejunior@gmail.com>
2025-09-27 13:46:17 +0000

    Desafio Novato SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,7 +27,7 @@ typedef struct {
 
   CartaSuperTrunfo carta_do_usuario;
 
-  printf("--- CRIAR NOVA CARTA SUPER TRUNFO ---\n\n");
+  printf("--- CRIAR PRIMEIRA CARTA SUPER TRUNFO ---\n\n");
 
   printf("Digite o estado: ");
   scanf("%s", carta_do_usuario.estado);
@@ -59,7 +59,7 @@ typedef struct {
 
 
   // Área para exibição dos dados da cidade
-printf("\n--- CARTA SUPER TRUNFO CRIADA ---\n");
+printf("\n--- PRIMEIRA CARTA SUPER TRUNFO CRIADA ---\n");
 
     printf("O estado é: %s\n", carta_do_usuario.estado);
     printf("o codigo da carta é: %s\n", carta_do_usuario.codigo);
@@ -70,11 +70,59 @@ printf("\n--- CARTA SUPER TRUNFO CRIADA ---\n");
     printf("O numero de pontos turisticos é: %d\n", carta_do_usuario.pontos_turisticos);
     
 
+    // ---SEGUNDA CARTA ---
+
+    printf("--- CRIAR SEGUNDA CARTA SUPER TRUNFO ---\n\n");
+
+     printf("Digite o estado: ");
+  scanf("%s", carta_do_usuario.estado);
+
+  printf("Digite a cidade: ");
+  scanf("%s", carta_do_usuario.cidade);
+
+  printf("digite a população: ");
+  scanf("%lld", &carta_do_usuario.populaçao);
+
+  getchar();
+
+  printf("Digite area (em km2): ");
+  scanf("%lf", &carta_do_usuario.area);
+
+  getchar();
+
+  printf("Digite o pib: ");
+  scanf("%lf", &carta_do_usuario.pib);
+
+  getchar();
+
+  printf("Digite o numero de pontos turisticos: ");
+  scanf("%d", &carta_do_usuario.pontos_turisticos);
+
+  getchar();
+
+
+  printf("\n--- SEGUNDA CARTA SUPER TRUNFO CRIADA ---\n");
+
+    printf("O estado é: %s\n", carta_do_usuario.estado);
+    printf("o codigo da carta é: %s\n", carta_do_usuario.codigo);
+    printf("A cidade é: %s\n", carta_do_usuario.cidade);
+    printf("A população é: %lld\n", carta_do_usuario.populaçao);
+    printf("A area é: %.2f km2\n", carta_do_usuario.area);
+    printf("O PIB é: %.2f\n", carta_do_usuario.pib);
+    printf("O numero de pontos turisticos é: %d\n", carta_do_usuario.pontos_turisticos);
+
+    return 0;
+
+ }
+    
 
 
 
 
 
-return 0;
 
-} 
+
+
+
+
+
